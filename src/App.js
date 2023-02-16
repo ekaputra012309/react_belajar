2d4c1cae17a4e2541d9f7e0e6f97c3698fad96b3
putra940616 <putra940616@gmail.com>
2023-02-16 11:16:16 +0700

    useState Hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import Header from "./components/Header";
+// import Header from "./components/Header";
+import { useState } from "react";
 
 function App() {
-  const clickMe = (name) => {
-    console.log('Hello ' + name);
+  // const clickMe = (name) => {
+  //   console.log('Hello ' + name);
+  // }
+  const [title, setTitle] = useState("Hello World");
+
+  const changeTitle = () => {
+    setTitle("Title Change");
   }
+
   return (
     <div>
-      <Header />
-      <button onClick={ () => clickMe('Eko')}>Click Me</button>
+      {/* <Header /> */}
+      <h1>{ title }</h1>
+      <button onClick={ changeTitle }>Change Title</button>
     </div>
   );
 }
